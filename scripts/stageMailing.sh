fa01ba8ab7fb9d9e7d5ecd743bc0462fbcff818a
enderpiyale <enderpiyale@gmail.com>
2020-01-14 23:17:39 +0300

    space

--- scripts/stageMailing.sh
+++ scripts/stageMailing.sh
@@ -2,4 +2,4 @@
 # exit script when any command ran here returns with non-zero exit code
 set -e
 
-curl --request POST --url https://api.sendgrid.com/v3/mail/send --header "Authorization: Bearer $SENDGRID_API_KEY"" --header 'Content-Type: application/json' --data '{"personalizations": [{"to": [{"email": "enderpiyale@gmail.com"}]}],"from": {"email": "enderpiyale@gmail.com"},"subject": “Stage Deployed,”content": [{"type": "text/plain", "value": “Stage workflow completed successfully!"}]}'
+curl --request POST --url https://api.sendgrid.com/v3/mail/send --header "Authorization: Bearer $SENDGRID_API_KEY" --header 'Content-Type: application/json' --data '{"personalizations": [{"to": [{"email": "enderpiyale@gmail.com"}]}],"from": {"email": "enderpiyale@gmail.com"},"subject": “Stage Deployed,”content": [{"type": "text/plain", "value": “Stage workflow completed successfully!"}]}'
