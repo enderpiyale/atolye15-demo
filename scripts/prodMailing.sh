b6ce6d153600043b74f231fda0619b94f669afb4
enderpiyale <enderpiyale@gmail.com>
2020-01-15 01:57:23 +0300

    SendGrid

--- scripts/prodMailing.sh
+++ scripts/prodMailing.sh
@@ -2,4 +2,4 @@
 # exit script when any command ran here returns with non-zero exit code
 set -e
 
-curl --request POST --url https://api.sendgrid.com/v3/mail/send --header "Authorization: Bearer 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" --header 'Content-Type: application/json' --data '{"personalizations": [{"to": [{"email": "enderpiyale@gmail.com"}]}],"from": {"email": "enderpiyale@gmail.com"},"subject": "Production Deployed", "content": [{"type": "text/plain", "value": "Production workflow completed successfully!"}]}'
+curl --request POST --url https://api.sendgrid.com/v3/mail/send --header "Authorization: Bearer $SENDGRID_API_KEY" --header 'Content-Type: application/json' --data '{"personalizations": [{"to": [{"email": "enderpiyale@gmail.com"}]}],"from": {"email": "enderpiyale@gmail.com"},"subject": "Production Deployed", "content": [{"type": "text/plain", "value": "Production workflow completed successfully!"}]}'
